78c5fb554861487424ab25b353f002af32b9e599
Preda3012 <58793781+Preda3012@users.noreply.github.com>
2020-06-02 15:54:26 +0200

    retiré l'ancien nav bar de l'HTML

--- SinamaPPE/pagefilmsetevenements.php
+++ SinamaPPE/pagefilmsetevenements.php
@@ -27,39 +27,7 @@
       ?>
     <!-- la navbar de l'accueil du site -->
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- controle la couleur de la nav bar-->
-
-<!-- <img src="logociné2.png" alt="" width="200" /> -->
-
-  <a class="navbar-brand" href="pageaccueilsinama.html"><img src="logocine2copie.png" width="170"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="pageaccueilsinama.php">ACCUEIL<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">FILMS & ÉVÈNEMENTS <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">OFFRES ET PROMOTIONS<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="pageAchatBillet.html">TARIFS<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">NOUS CONTACTER<span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-    </form>
-  </div>
-</nav>
+    
 <?php
     echo "string";
 
