b4faddec0d5afba5fc6378f221e91c1998c66a2b
Preda3012 <58793781+Preda3012@users.noreply.github.com>
2020-06-02 16:26:56 +0200

    page tarif -> .php

--- SinamaPPE/tarifs.php
+++ SinamaPPE/tarifs.php
@@ -22,37 +22,14 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <!-- la navbar de l'accueil du site -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" width="170"> <!-- controle la couleur de la nav bar-->
-  <a class="navbar-brand" href="pageaccueilsinama.html"><img src="logocine2copie.png" width="170"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="pageaccueilsinama.html">ACCUEIL<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">FILMS & ÉVÈNEMENTS <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">OFFRES ET PROMOTIONS<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="tarifs.html">TARIFS<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="contactersinama.html">NOUS CONTACTER<span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-    </form>
-  </div>
-</nav>
+    <?php
+        include 'scriptphp/bandeau.php';
+      ?>
+
+
+    <!-- la navbar de l'accueil du site -->
+
 
 <!-- image qui défile sur les évènements et autres PROMOTIONS et OFFRES.... exactement-->
 
