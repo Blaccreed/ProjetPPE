e1f35c5762601fc79081fb00bab3cf49d18e9ea2
Preda3012 <58793781+Preda3012@users.noreply.github.com>
2020-06-08 22:02:18 +0200

    n

--- SinamaPPE/pageconnexion.php
+++ SinamaPPE/pageconnexion.php
@@ -13,7 +13,7 @@
 
   <body>
     <!-- je veux un bouton pour qu'il puisse s'inscrire au dessus de la barre ! -->
-    <?php include 'scriptphp/Statut.php'; ?>
+<?php include 'scriptphp/Statut.php'; ?>
     <img src="imagedivers/logociné2.png" alt="" width="200" />
 
     <!-- Optional JavaScript -->
